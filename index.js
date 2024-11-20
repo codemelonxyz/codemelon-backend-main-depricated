3e0e6cff6a263caf97fff479c4f8d248b1052238
Vaiditya2207 <vaiditya.t23csai@nst.rishihood.edu.in>
2024-11-20 13:32:18 +0530

    cors test 2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv';
-// import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 // import serverAuth from './middlewares/server.auth.js';
-import serverHealthRoutes from './routes/serverhealth.routes.js';
-import authRoutes from './routes/auth.routes.js';
+import serverHealthRoutes from "./routes/serverhealth.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -11,37 +11,32 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 // Define allowed origins
-const allowedOrigins = [
-  'https://codemelon.xyz',
-  'http://localhost:3000'
-];
+const allowedOrigins = ["https://codemelon.xyz", "http://localhost:3000"];
 
 // CORS configuration
 // const corsOptions = {
 //   origin: true, // Allow all origins
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: '*', // Allow all headers
-//   credentials: true,
-//   optionsSuccessStatus: 204
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: "*", // Allow all headers
 // };
 
 // Global Middlewares
-// app.use(cors()); // Allow all origins, methods, and headers
+app.use(cors()); // Allow all origins, methods, and headers
 app.use(express.json());
 
 // Handle OPTIONS requests globally
 // app.options('*', cors(corsOptions));
 
 // Routes
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
     message: "Welcome to codemelon APIs",
-    instruction: "Go to codemelon.xyz/developers for more"
+    instruction: "Go to codemelon.xyz/developers for more",
   });
 });
 
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1', serverHealthRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1", serverHealthRoutes);
 
 // Apply serverAuth to protected routes if necessary
 // app.use('/api/v1/protected', serverAuth, protectedRoutes);
