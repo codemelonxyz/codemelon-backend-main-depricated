a3b79df557888198ad86d62c1691c5b1e7b23023
Vaiditya Tanwar <vaidityatanwar2207@gmail.com>
2024-11-23 02:18:31 +0530

    fix

--- index.js
+++ index.js
@@ -4,29 +4,16 @@ import cors from "cors";
 // import serverAuth from './middlewares/server.auth.js';
 import serverHealthRoutes from "./routes/serverhealth.routes.js";
 import authRoutes from "./routes/auth.routes.js";
-import waitlisRoutes from "./routes/waitlist.routes.js";
+import waitlistRoutes from "./routes/waitlist.routes.js"; // Corrected import
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const app = express();
-
-// Define allowed origins
-// const allowedOrigins = ["https://codemelon.xyz", "http://localhost:3000"];
-
-// CORS configuration
-// const corsOptions = {
-//   origin: true, // Allow all origins
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: "*", // Allow all headers
-// };
 
 // Global Middlewares
 app.use(cors()); // Allow all origins, methods, and headers
 app.use(express.json());
-
-// Handle OPTIONS requests globally
-// app.options('*', cors(corsOptions));
 
 // Routes
 app.get("/", (req, res) => {
@@ -38,7 +25,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1", serverHealthRoutes);
-app.use("/api/v1", waitlisRoutes);
+app.use("/api/v1", waitlistRoutes); // Corrected usage
 
 // Apply serverAuth to protected routes if necessary
 // app.use('/api/v1/protected', serverAuth, protectedRoutes);
